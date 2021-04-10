afddf0f8f910ae01c14392cbaa780c9255ef944e
Salika Dave <salikadave26@gmail.com>
2021-04-10 20:22:29 +0530

    add T5-base and T5-large transformer

--- summarizer.py
+++ summarizer.py
@@ -1,7 +1,8 @@
 # YouTube API
 from youtube_transcript_api import YouTubeTranscriptApi
 import json
-from transformers import pipeline
+# from transformers import pipeline
+from transformers import T5ForConditionalGeneration, T5Tokenizer
 
 
 # Return transcripts from Video ID
@@ -21,17 +22,28 @@ def fetch_video_transcripts(video_id):
     except:
         return 'Transcripts not found!'
     
-# convert transcript to abstractive summary
+# convert using pipeline api
+# def transcript_to_summary(video_transcript):
+#     # using pipeline API for summarization task
+#     summarization = pipeline("summarization")
+#     summary_text = summarization(video_transcript)[0]['summary_text']
+#     print("Summary:", summary_text)
+
+# convert using T5 transformer
 def transcript_to_summary(video_transcript):
-    # using pipeline API for summarization task
-    summarization = pipeline("summarization")
-    summary_text = summarization(video_transcript)[0]['summary_text']
-    print("Summary:", summary_text)
+    model = T5ForConditionalGeneration.from_pretrained("t5-base")
+    tokenizer = T5Tokenizer.from_pretrained("t5-base")
+    # T5 uses a max_length of 512 so we cut the article to 512 tokens.
+    inputs = tokenizer.encode("summarize: " + video_transcript, return_tensors="pt", max_length=512, truncation=True)
+    outputs = model.generate(inputs, max_length=500, min_length=40, length_penalty=3.0, num_beams=4, early_stopping=True)
+    print(outputs)
+    print(tokenizer.decode(outputs[0]))
+
 
 
 
 transcipts_under_1024 = ["NiKtZgImdlY","_Nq4Z5i7lcs","ZyYqyYAKGC0","yj8sAnZ6c3g"]
-transcript = fetch_video_transcripts(transcipts_under_1024[3])
+transcript = fetch_video_transcripts(transcipts_under_1024[0])
 print("="*50)
 transcript_to_summary(transcript)
 
