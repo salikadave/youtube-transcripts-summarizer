89819e16062dd8d0f9ad354f229bfe25cf10eb01
Salika Dave <salikadave26@gmail.com>
2021-04-10 11:57:13 +0530

    complete fetch transcript from API with error handling

--- summarizer.py
+++ summarizer.py
@@ -1,11 +1,26 @@
 # YouTube API
 from youtube_transcript_api import YouTubeTranscriptApi
+import json
 
 
 # Return transcripts from Video ID
-def fetch_video_subtitles(video_id):
-    subtitles = YouTubeTranscriptApi.get_transcript(video_id)
-    return subtitles
+def fetch_video_transcripts(video_id):
+    video_summary = []
+    try:
+        transcripts_arr = YouTubeTranscriptApi.get_transcript(video_id)
+        for elem in transcripts_arr:
+            # print(elem)
+            text = elem["text"]
+            elem = text.replace('\n'," ")
+            # elem = text.replace('\t',"")
+            # print(elem)
+            video_summary.append(elem)
+        video_summary_string = " ".join(video_summary)
+        return video_summary_string
+    except:
+        return 'Transcripts not found!'
+    
 
-video_id = "Hu4Yvq-g7_Y"
-print(fetch_video_subtitles(video_id))
+
+video_id = "emBoDloCze8"
+transcript = fetch_video_transcripts(video_id)
