786fec07449b0c4b9849fc04923edefd2fedfe06
Salika Dave <salikadave26@gmail.com>
2021-04-11 17:54:47 +0530

    add route to return transcript summaries

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 # adding flask boilerplate for REST APIs
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 from datetime import datetime
 import os
 import sys
+import json
+from flask import make_response
+# Import Custom Module
+import summarizer
 
 
 # define a variable to hold your app
@@ -17,9 +21,23 @@ def index_page():
 def get_time():
     return str(datetime.datetime.now())
 
+@app.route('/api/summarize', methods=["GET"])
+def perform_summarization():
+    youtube_url = request.args.get('youtube')
+    video_id = youtube_url.split("=")
+    video_transcript = summarizer.fetch_video_transcripts(video_id[1])
+    transcript_summary = summarizer.transcript_to_summary(video_transcript)
+    return json.dumps(transcript_summary, indent = 4)
+    # return json.dumps(video_url,indent=4)
+
+@app.errorhandler(404)
+def not_found(error):
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
 # serve the app when this file is run
 if __name__=='__main__':
     app.run(use_reloader=True)
     print('server running')
-    print('hello')
+
+
 
--- summarizer.py
+++ summarizer.py
@@ -1,8 +1,8 @@
 # YouTube API
 from youtube_transcript_api import YouTubeTranscriptApi
 import json
-# from transformers import pipeline
-from transformers import T5ForConditionalGeneration, T5Tokenizer
+from transformers import pipeline
+# from transformers import T5ForConditionalGeneration, T5Tokenizer
 
 
 # Return transcripts from Video ID
@@ -13,39 +13,39 @@ def fetch_video_transcripts(video_id):
         for elem in transcripts_arr:
             # print(elem)
             text = elem["text"]
-            elem = text.replace('\n'," ")
+            elem = text.replace("\n",' ')
             # elem = text.replace('\t',"")
             # print(elem)
             video_summary.append(elem)
-        video_summary_string = " ".join(video_summary)
+        separator = " "
+        video_summary_string = separator.join(video_summary)
         return video_summary_string
     except:
         return 'Transcripts not found!'
     
 # convert using pipeline api
-# def transcript_to_summary(video_transcript):
-#     # using pipeline API for summarization task
-#     summarization = pipeline("summarization")
-#     summary_text = summarization(video_transcript)[0]['summary_text']
-#     print("Summary:", summary_text)
+def transcript_to_summary(video_transcript):
+    # using pipeline API for summarization task
+    summarization = pipeline("summarization")
+    summary_text = summarization(video_transcript)[0]['summary_text']
+    # print("Summary:", summary_text)
+    return summary_text
 
 # convert using T5 transformer
-def transcript_to_summary(video_transcript):
-    model = T5ForConditionalGeneration.from_pretrained("t5-base")
-    tokenizer = T5Tokenizer.from_pretrained("t5-base")
-    # T5 uses a max_length of 512 so we cut the article to 512 tokens.
-    inputs = tokenizer.encode("summarize: " + video_transcript, return_tensors="pt", max_length=512, truncation=True)
-    outputs = model.generate(inputs, max_length=500, min_length=40, length_penalty=3.0, num_beams=4, early_stopping=True)
-    print(outputs)
-    print(tokenizer.decode(outputs[0]))
+# def transcript_to_summary(video_transcript):
+#     model = T5ForConditionalGeneration.from_pretrained("t5-base")
+#     tokenizer = T5Tokenizer.from_pretrained("t5-base")
+#     # T5 uses a max_length of 512 so we cut the article to 512 tokens.
+#     inputs = tokenizer.encode("summarize: " + video_transcript, return_tensors="pt", max_length=512, truncation=True)
+#     outputs = model.generate(inputs, max_length=500, min_length=40, length_penalty=3.0, num_beams=4, early_stopping=True)
+#     print(outputs)
+#     print(tokenizer.decode(outputs[0]))
 
-
-
-
-transcipts_under_1024 = ["NiKtZgImdlY","_Nq4Z5i7lcs","ZyYqyYAKGC0","yj8sAnZ6c3g"]
-transcript = fetch_video_transcripts(transcipts_under_1024[0])
-print("="*50)
-transcript_to_summary(transcript)
+# Sample
+# transcipts_under_1024 = ["NiKtZgImdlY","_Nq4Z5i7lcs","ZyYqyYAKGC0","yj8sAnZ6c3g","1K5SycZjGhI"]
+# transcript = fetch_video_transcripts(transcipts_under_1024[0])
+# print("="*50)
+# transcript_to_summary(transcript)
 
 
 
