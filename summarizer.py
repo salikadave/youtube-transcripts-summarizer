4411984a941b0e19cd89bcd6ac4c5fbc2b5291c6
Salika Dave <salikadave26@gmail.com>
2021-04-20 19:27:40 +0530

    add print statements in summarization process

--- summarizer.py
+++ summarizer.py
@@ -2,7 +2,7 @@
 from youtube_transcript_api import YouTubeTranscriptApi
 import json
 from transformers import pipeline
-# from transformers import T5ForConditionalGeneration, T5Tokenizer
+from transformers import T5ForConditionalGeneration, T5Tokenizer
 
 
 # Return transcripts from Video ID
@@ -24,28 +24,31 @@ def fetch_video_transcripts(video_id):
         return 'Transcripts not found!'
     
 # convert using pipeline api
-def transcript_to_summary(video_transcript):
+def transcript_to_summary_pipeline(video_transcript):
     # using pipeline API for summarization task
+    print("Using Pipeline API. . . .")
     summarization = pipeline("summarization")
+    print("Preparing summary. . .")
     summary_text = summarization(video_transcript)[0]['summary_text']
-    # print("Summary:", summary_text)
+    print("Summary:", summary_text)
     return summary_text
 
 # convert using T5 transformer
-# def transcript_to_summary(video_transcript):
-#     model = T5ForConditionalGeneration.from_pretrained("t5-base")
-#     tokenizer = T5Tokenizer.from_pretrained("t5-base")
-#     # T5 uses a max_length of 512 so we cut the article to 512 tokens.
-#     inputs = tokenizer.encode("summarize: " + video_transcript, return_tensors="pt", max_length=512, truncation=True)
-#     outputs = model.generate(inputs, max_length=500, min_length=40, length_penalty=3.0, num_beams=4, early_stopping=True)
-#     print(outputs)
-#     print(tokenizer.decode(outputs[0]))
+def transcript_to_summary_t5(video_transcript):
+    model = T5ForConditionalGeneration.from_pretrained("t5-base")
+    tokenizer = T5Tokenizer.from_pretrained("t5-base")
+    # T5 uses a max_length of 512 so we cut the article to 512 tokens.
+    inputs = tokenizer.encode("summarize: " + video_transcript, return_tensors="pt", max_length=512, truncation=True)
+    outputs = model.generate(inputs, max_length=500, min_length=40, length_penalty=4.0, num_beams=4, early_stopping=True)
+    print(outputs)
+    print(tokenizer.decode(outputs[0]))
 
 # Sample
-# transcipts_under_1024 = ["NiKtZgImdlY","_Nq4Z5i7lcs","ZyYqyYAKGC0","yj8sAnZ6c3g","1K5SycZjGhI"]
-# transcript = fetch_video_transcripts(transcipts_under_1024[0])
-# print("="*50)
-# transcript_to_summary(transcript)
+transcipts_under_1024 = ["NiKtZgImdlY","_Nq4Z5i7lcs","ZyYqyYAKGC0","yj8sAnZ6c3g","1K5SycZjGhI"]
+transcript = fetch_video_transcripts(transcipts_under_1024[1])
+print("="*50)
+# transcript_to_summary_pipeline(transcript)
+transcript_to_summary_t5(transcript)
 
 
 
